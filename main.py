49dff2f45a8cf84d9791fe612875ed00c87b0223
Sajal Arora <sajalarora007@gmail.com>
2019-04-02 18:54:16 +0530

    Updated git fault

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 import json
 from flask import Flask, request, jsonify
 import sqlite3
-
+ 
 app = Flask(__name__)
-
+ 
 # class for sorting...
 class Sortify:
     # configure database
@@ -12,7 +12,7 @@ class Sortify:
         self.cur = sqlite3.connect('Sortify.db')
         self.cur.execute('drop table if exists Dict')
         self.cur.execute('CREATE TABLE IF NOT EXISTS Dict (id integer, name text);')
-    
+   
     # Stores the request json into db
     def json_to_dict(self, data):
         listt = json.loads(data)
@@ -20,10 +20,10 @@ class Sortify:
             x = i['id']
             y = i['name']
             self.cur.execute('INSERT INTO dict (id, name) values (?, ?);', (x, int(y[1:]),))
-
+ 
     # fetches the data from db and sorts the values of a dict in a list
     def sorted_list(self):
-        result = self.cur.execute('SELECT * FROM dict ORDER BY name ASC')
+        result = self.cur.execute('SELECT * FROM dict')
         listt = list()
         for k, v in result:
             print(k,v)
@@ -31,8 +31,16 @@ class Sortify:
             dictt['id'] = k
             dictt['name'] = 'a'+ str(v)
             listt.append(dictt)
-        return listt
-
+        return rev(listt)
+ 
+# Reversing list of dict
+def rev(data):
+    new_list = list()
+    for i in range(len(data)-1, -1, -1):
+        new_list.append(data[i])
+    return new_list
+ 
+ 
 # Route to return sorted json
 @app.route('/sort_dict', methods=['POST'])
 def sort_dict_db():
@@ -42,6 +50,6 @@ def sort_dict_db():
     sorty.json_to_dict(request.data)
     x = sorty.sorted_list()
     return jsonify(x), 200 #Returns JSON with status code of 200
-
+ 
 if '__name__' == '__main__':
     app.run(debug=true)
