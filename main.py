c00a101565d2067dfa09dd40ecaf2b1efc1eefdf
Sajal Arora <sajalarora007@gmail.com>
2019-04-02 19:15:24 +0530

    code optimization

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class Sortify:
     def __init__(self):
         self.cur = sqlite3.connect('Sortify.db')
         self.cur.execute('drop table if exists Dict')
-        self.cur.execute('CREATE TABLE IF NOT EXISTS Dict (id integer, name text);')
+        self.cur.execute('CREATE TABLE IF NOT EXISTS Dict (id integer, name integer);')
    
     # Stores the request json into db
     def json_to_dict(self, data):
@@ -23,7 +23,7 @@ class Sortify:
  
     # fetches the data from db and sorts the values of a dict in a list
     def sorted_list(self):
-        result = self.cur.execute('SELECT * FROM dict')
+        result = self.cur.execute('SELECT id, name FROM Dict order by name ASC')
         listt = list()
         for k, v in result:
             print(k,v)
@@ -31,16 +31,13 @@ class Sortify:
             dictt['id'] = k
             dictt['name'] = 'a'+ str(v)
             listt.append(dictt)
-        return rev(listt)
+        return listt
  
-# Reversing list of dict
-def rev(data):
-    new_list = list()
-    for i in range(len(data)-1, -1, -1):
-        new_list.append(data[i])
-    return new_list
- 
- 
+ # Sample input
+l = [{'id': 92, 'name': 'a21'}, {'id': 12, 'name': 'a10'}, {'id': 15, 'name': 'a5'}, {'id': 72, 'name': 'a82'}]
+ # sample output
+t = [{'id': 15, 'name': 'a5'}, {'id': 12, 'name': 'a10'}, {'id': 92, 'name': 'a21'}, {'id': 72, 'name': 'a82'}]
+
 # Route to return sorted json
 @app.route('/sort_dict', methods=['POST'])
 def sort_dict_db():
